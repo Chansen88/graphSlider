b494eedce8b8530f93353df9207268194fc15191
Chris Hansen <Chansen88@me.com>
2016-02-16 15:33:48 -0800

    Create Comment object and added time to comments

--- services/graphs.js
+++ services/graphs.js
@@ -13,6 +13,11 @@
           this.comments = [];
         };
 
+        function Comment(msg) {
+          this.msg = msg;
+          this.time = Date.now();
+        }
+
         Graphs.populate = function() {
           Graphs.graphs.push(new Graph('http://www.stockmarket-coach.com/images/reading-stock-charts-candlestick-chart.jpg'));
           Graphs.graphs.push(new Graph('http://www.simple-stock-trading.com/wp-content/uploads/2015/06/ibm_stock_chart_is_example_of_online_stock_charts.png'));
@@ -23,8 +28,8 @@
           Graphs.current[0] = graph;
         };
 
-        Graphs.addComment = function(comment) {
-          Graphs.current[0].comments.push(comment);
+        Graphs.addComment = function(msg) {
+          Graphs.current[0].comments.push(new Comment(msg));
         };
 
         Graphs.removeComment= function(index) {
